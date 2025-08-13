317b7779ae3515ea36d56d1c7dbe16a251e1421f
David Rivers <voxfox01@gmail.com>
2025-08-13 12:47:26 -0700

    Updated credentials v2

--- parking_app/__init__.py
+++ parking_app/__init__.py
@@ -3,12 +3,33 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import OperationalError
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+
+def _ensure_database(database_uri: str) -> None:
+    """Create the database specified in ``database_uri`` if it does not exist."""
+    engine = create_engine(database_uri)
+    try:
+        engine.connect().close()
+    except OperationalError:
+        url = engine.url
+        default_url = url.set(database="postgres")
+        tmp_engine = create_engine(default_url)
+        conn = tmp_engine.connect()
+        conn.execution_options(isolation_level="AUTOCOMMIT").execute(
+            text(f"CREATE DATABASE {url.database}")
+        )
+        conn.close()
+        tmp_engine.dispose()
+    finally:
+        engine.dispose()
 
 
 def create_app(config_object=None):
@@ -25,6 +46,8 @@ def create_app(config_object=None):
     )
     if config_object:
         app.config.from_object(config_object)
+
+    _ensure_database(app.config['SQLALCHEMY_DATABASE_URI'])
 
     db.init_app(app)
     login_manager.init_app(app)
