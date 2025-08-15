160552b3d31d6b8deebc5bb6ca1c94cbcf99c607
David Rivers <voxfox01@yahoo.com>
2025-08-15 15:16:50 -0700

    Add signup routes and owner dashboard

--- parking_app/models.py
+++ parking_app/models.py
@@ -1,28 +1,123 @@
 from datetime import datetime
-from . import db
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 
-class Property(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), nullable=False)
-    address = db.Column(db.String(256))
-    vehicles = db.relationship('Vehicle', backref='property', lazy=True)
+class Type2Mixin:
+    """Mixin that provides Type 2 slowly changing dimension fields."""
+
+    Creation_Date = db.Column(db.DateTime, default=datetime.utcnow)
+    Modification_Date = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+    Current_Flag = db.Column(db.Boolean, default=True)
+    Status_ID = db.Column(db.Integer, db.ForeignKey("Status_CD.Status_ID"), default=1)
 
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
-    role = db.Column(db.String(50), nullable=False)
-    property_id = db.Column(db.Integer, db.ForeignKey('property.id'))
+class StatusCD(db.Model):
+    __tablename__ = "Status_CD"
+
+    Status_ID = db.Column(db.Integer, primary_key=True)
+    CD_Code = db.Column(db.String(1), unique=True, nullable=False)
+    Description = db.Column(db.String(50), nullable=False)
 
 
-class Vehicle(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    license_plate = db.Column(db.String(20), nullable=False)
-    apartment = db.Column(db.String(20))
-    is_guest = db.Column(db.Boolean, default=True)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    property_id = db.Column(db.Integer, db.ForeignKey('property.id'))
+class PropertyCustomer(db.Model, Type2Mixin):
+    __tablename__ = "Property_Customers"
+
+    Property_ID = db.Column(db.Integer, primary_key=True)
+    Property_Name = db.Column(db.String(128), nullable=False)
+    Property_Address = db.Column(db.String(256))
+    Property_City = db.Column(db.String(100))
+    Property_State = db.Column(db.String(2))
+    Property_Zip = db.Column(db.String(10))
+
+    users = db.relationship("User", backref="property", lazy=True)
+    parking_customers = db.relationship("ParkingCustomer", backref="property", lazy=True)
+    security_links = db.relationship(
+        "SecurityProperty", backref="property", lazy=True
+    )
+
+
+class SecurityCustomer(db.Model, Type2Mixin):
+    __tablename__ = "Security_Customers"
+
+    Security_ID = db.Column(db.Integer, primary_key=True)
+    Security_Name = db.Column(db.String(128), nullable=False)
+    Security_City = db.Column(db.String(100))
+    Security_State = db.Column(db.String(2))
+    Security_Zip = db.Column(db.String(10))
+
+    users = db.relationship("User", backref="security", lazy=True)
+    properties = db.relationship("SecurityProperty", backref="security", lazy=True)
+
+
+class SecurityProperty(db.Model, Type2Mixin):
+    __tablename__ = "Security_Properties_dim"
+
+    Security_Property_ID = db.Column(db.Integer, primary_key=True)
+    Security_ID = db.Column(
+        db.Integer, db.ForeignKey("Security_Customers.Security_ID"), nullable=False
+    )
+    Property_ID = db.Column(
+        db.Integer, db.ForeignKey("Property_Customers.Property_ID"), nullable=False
+    )
+
+
+class User(db.Model, UserMixin, Type2Mixin):
+    __tablename__ = "Users"
+
+    User_ID = db.Column(db.Integer, primary_key=True)
+    Security_ID = db.Column(db.Integer, db.ForeignKey("Security_Customers.Security_ID"))
+    Property_ID = db.Column(db.Integer, db.ForeignKey("Property_Customers.Property_ID"))
+    First_Name = db.Column(db.String(50))
+    Last_Name = db.Column(db.String(50))
+    Role = db.Column(db.String(50), nullable=False)
+
+    credentials = db.relationship("Credential", backref="user", uselist=False)
+    monitors = db.relationship("ParkingMonitoring", backref="user", lazy=True)
+
+    def get_id(self) -> str:  # pragma: no cover - simple accessor
+        return str(self.User_ID)
+
+
+class Credential(db.Model, Type2Mixin):
+    __tablename__ = "Credentials"
+
+    Credential_ID = db.Column(db.Integer, primary_key=True)
+    User_ID = db.Column(db.Integer, db.ForeignKey("Users.User_ID"), nullable=False)
+    Username = db.Column(db.String(80), unique=True, nullable=False)
+    Password_Hash = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, password: str) -> None:
+        self.Password_Hash = generate_password_hash(password)
+
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.Password_Hash, password)
+
+
+class ParkingCustomer(db.Model, Type2Mixin):
+    __tablename__ = "Parking_Customers"
+
+    Parking_Customer_ID = db.Column(db.Integer, primary_key=True)
+    Property_ID = db.Column(
+        db.Integer, db.ForeignKey("Property_Customers.Property_ID"), nullable=False
+    )
+    First_Name = db.Column(db.String(50))
+    Last_Name = db.Column(db.String(50))
+    Unit = db.Column(db.String(20))
+    License_Plate = db.Column(db.String(20), nullable=False)
+    Departure_Date = db.Column(db.DateTime, nullable=False)
+
+
+class ParkingMonitoring(db.Model, Type2Mixin):
+    __tablename__ = "Parking_Monitoring"
+
+    Parking_Monitor_ID = db.Column(db.Integer, primary_key=True)
+    User_ID = db.Column(db.Integer, db.ForeignKey("Users.User_ID"))
+    Property_ID = db.Column(
+        db.Integer, db.ForeignKey("Property_Customers.Property_ID"), nullable=False
+    )
+    License_Plate = db.Column(db.String(20), nullable=False)
+    Registered_Flag = db.Column(db.Boolean, default=False)
--- parking_app/routes.py
+++ parking_app/routes.py
@@ -1,6 +1,15 @@
+from datetime import datetime
 from flask import Blueprint, render_template, redirect, url_for, request
-from flask_login import login_user, logout_user, login_required
-from .models import db, User, Property, Vehicle
+from flask_login import login_user, logout_user, login_required, current_user
+from .models import (
+    db,
+    User,
+    PropertyCustomer,
+    ParkingCustomer,
+    SecurityCustomer,
+    SecurityProperty,
+    Credential,
+)
 from . import login_manager
 
 bp = Blueprint('main', __name__)
@@ -13,40 +22,96 @@ def load_user(user_id):
 
 @bp.route('/')
 def index():
-    properties = Property.query.all()
+    properties = PropertyCustomer.query.all()
     return render_template('index.html', properties=properties)
 
 
 @bp.route('/register_vehicle', methods=['GET', 'POST'])
-@login_required
 def register_vehicle():
     if request.method == 'POST':
-        plate = request.form['license_plate']
-        apartment = request.form.get('apartment')
         prop_id = request.form.get('property_id')
-        is_guest = request.form.get('is_guest') == 'on'
-        vehicle = Vehicle(
-            license_plate=plate,
-            apartment=apartment,
-            property_id=prop_id,
-            user_id=request.form.get('user_id'),
-            is_guest=is_guest,
+        first = request.form.get('first_name')
+        last = request.form.get('last_name')
+        unit = request.form.get('unit')
+        plate = request.form.get('license_plate')
+        departure = datetime.strptime(request.form['departure_date'], '%Y-%m-%d')
+
+        customer = ParkingCustomer(
+            Property_ID=prop_id,
+            First_Name=first,
+            Last_Name=last,
+            Unit=unit,
+            License_Plate=plate,
+            Departure_Date=departure,
         )
-        db.session.add(vehicle)
+        db.session.add(customer)
         db.session.commit()
         return redirect(url_for('main.index'))
 
-    properties = Property.query.all()
+    properties = PropertyCustomer.query.all()
     return render_template('register_vehicle.html', properties=properties)
+
+
+@bp.route('/signup/property', methods=['GET', 'POST'])
+def signup_property():
+    if request.method == 'POST':
+        name = request.form['property_name']
+        username = request.form['username']
+        password = request.form['password']
+
+        prop = PropertyCustomer(Property_Name=name)
+        db.session.add(prop)
+        db.session.flush()
+
+        user = User(Role='property_owner', Property_ID=prop.Property_ID)
+        db.session.add(user)
+        db.session.flush()
+
+        cred = Credential(User_ID=user.User_ID, Username=username)
+        cred.set_password(password)
+        db.session.add(cred)
+        db.session.commit()
+
+        login_user(user)
+        return redirect(url_for('main.owner_dashboard'))
+
+    return render_template('signup_property.html')
+
+
+@bp.route('/signup/security', methods=['GET', 'POST'])
+def signup_security():
+    if request.method == 'POST':
+        name = request.form['security_name']
+        username = request.form['username']
+        password = request.form['password']
+
+        sec = SecurityCustomer(Security_Name=name)
+        db.session.add(sec)
+        db.session.flush()
+
+        user = User(Role='security_admin', Security_ID=sec.Security_ID)
+        db.session.add(user)
+        db.session.flush()
+
+        cred = Credential(User_ID=user.User_ID, Username=username)
+        cred.set_password(password)
+        db.session.add(cred)
+        db.session.commit()
+
+        login_user(user)
+        return redirect(url_for('main.index'))
+
+    return render_template('signup_security.html')
 
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
-        user = User.query.filter_by(username=username).first()
-        if user:
-            login_user(user)
+        password = request.form['password']
+        cred = Credential.query.filter_by(Username=username, Current_Flag=True).first()
+        if cred and cred.check_password(password):
+            login_user(cred.user)
             return redirect(url_for('main.index'))
     return render_template('login.html')
 
@@ -56,3 +121,44 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('main.index'))
+
+
+@bp.route('/owner_dashboard', methods=['GET', 'POST'])
+@login_required
+def owner_dashboard():
+    if current_user.Role != 'property_owner':
+        return redirect(url_for('main.index'))
+
+    property_obj = current_user.property
+    security_companies = SecurityCustomer.query.all()
+    assigned = SecurityProperty.query.filter_by(
+        Property_ID=property_obj.Property_ID, Current_Flag=True
+    ).all()
+
+    if request.method == 'POST':
+        security_id = request.form.get('security_id')
+        if security_id:
+            existing = SecurityProperty.query.filter_by(
+                Property_ID=property_obj.Property_ID,
+                Security_ID=security_id,
+                Current_Flag=True,
+            ).first()
+            if not existing:
+                link = SecurityProperty(
+                    Property_ID=property_obj.Property_ID, Security_ID=security_id
+                )
+                db.session.add(link)
+                db.session.commit()
+            return redirect(url_for('main.owner_dashboard'))
+
+    reg_count = ParkingCustomer.query.filter_by(
+        Property_ID=property_obj.Property_ID
+    ).count()
+
+    return render_template(
+        'owner_dashboard.html',
+        property=property_obj,
+        security_companies=security_companies,
+        assigned=assigned,
+        registered_count=reg_count,
+    )
